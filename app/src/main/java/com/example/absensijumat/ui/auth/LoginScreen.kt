555d8065cfd1205821288edd025397e33a815d88
fanaardie25 <follgrowthindonesia@gmail.com>
2026-03-13 08:54:31 +0700

    feat: menambahkan icon mata di input field password

--- app/src/main/java/com/example/absensijumat/ui/auth/LoginScreen.kt
+++ app/src/main/java/com/example/absensijumat/ui/auth/LoginScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -29,11 +30,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -102,15 +105,33 @@ fun LoginScreen(
 
         Spacer(modifier = Modifier.height(16.dp))
 
+        var passwordVisible by remember { mutableStateOf(false) }
+
         OutlinedTextField(
             value = password,
             onValueChange = { password = it },
             label = { Text(text = "Password") },
             placeholder = { Text(text = "*******") },
             leadingIcon = { Icon(Icons.Default.Lock, contentDescription = null) },
+
+            trailingIcon = {
+                val image = if (passwordVisible)
+                    painterResource(R.drawable.eye_svgrepo_com)
+                else
+                    painterResource(R.drawable.eye_slash_svgrepo_com)
+
+                val description = if (passwordVisible) "Hide password" else "Show password"
+
+                IconButton(onClick = { passwordVisible = !passwordVisible },modifier = modifier.size(24.dp)) {
+                    Icon(painter = image, contentDescription = description)
+                }
+            },
+
             modifier = Modifier.fillMaxWidth(),
             shape = RoundedCornerShape(12.dp),
-            visualTransformation = PasswordVisualTransformation(),
+
+            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
         )
 
